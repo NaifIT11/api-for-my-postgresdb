933b833f88a76a803bca88cf01303963f90d26c6
Naif Otibi <nn.n@outlook.cl>
2024-10-03 14:32:36 +0300

    setting auth and last validation

--- server.js
+++ server.js
@@ -8,6 +8,41 @@ const app = express();
 
 const sql = postgres(process.env.POSTGRES_URL)
 
+
+
+app.get("/data" , async(req , res) => {
+    const authorization  = req.headers["authorization"];
+
+    if(!authorization){
+        res.status(401).json({
+            error:{
+                status: 401,
+                msessage: "Authorization header must be included"
+            }
+        })
+    }
+
+    const token = authorization.split(" ")[1];
+
+    if(token !== process.env.API_KEY){
+        res.status(401).json({
+            error: {
+                status: 401,
+                message: "Token invalid"
+            }
+        })
+    }
+
+
+    const customers = await sql`SELECT * FROM Customers`;
+
+    res.status(200).json({
+        data: customers
+    })
+
+})
+
+
 //use json parser for all routes
 app.use(bodyParser.json());
 
