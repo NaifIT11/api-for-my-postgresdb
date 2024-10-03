ee9ca8e6d8a82e3ef46942c28153708128f51e21
Naif Otibi <nn.n@outlook.cl>
2024-10-03 14:24:50 +0300

    setting the connection

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import dotenv from "dotenv";
 import express from "express"
 import bodyParser from "body-parser";
+import postgres from "postgres";
 dotenv.config()
 
 const app = express();
 
+const sql = postgres(process.env.POSTGRES_URL)
+
+//use json parser for all routes
+app.use(bodyParser.json());
 
